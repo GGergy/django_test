31bb042fb5e2ed9431b50a8851f7a0b86e4206b6
Gergy <gergy2k07@gmail.com>
2023-10-03 21:58:40 +0300

    fix req  4

--- lyceum/lyceum/settings.py
+++ lyceum/lyceum/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 load_dotenv()
-SECRET_KEY = os.environ['SECRET']
+SECRET_KEY = os.environ["SECRET"]
 print(SECRET_KEY)
 
 # SECURITY WARNING: don't run with debug turned on in production!
